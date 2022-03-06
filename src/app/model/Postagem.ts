ba3ccbe2e302c372356ee4ccbdb6671beef52b84
Gabriel Luis Nolasco <gabrielluisnolascooficial@gmail.com>
2022-03-06 15:19:55 -0300

    bug fix

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -3,8 +3,8 @@ import { User } from './User';
 
 export class Postagem {
   public id: number;
-  public titulo: string;
-  public texto: string;
+  public tituloPostagem: string;
+  public textoPostagem: string;
   public data: Date;
   public usuario: User;
   public tema: Tema;
